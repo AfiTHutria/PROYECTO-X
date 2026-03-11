8171530957688570961ea71194f20556f95f8dec
notardr <ardrnot@gamil.comm>
2026-03-11 15:29:10 -0500

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,useNavigate, useLocation } from "react-router-dom";
 import Home from "./presentation/pages/Home.jsx";
 import "../src/App.css";
 import Registro from "./presentation/pages/Registro.jsx";
 import Login from "./presentation/pages/Login.jsx";
 
 export default function App() {
+  const location= useLocation();
+  const background = location.state && location.state.background;
   return (
     <>
-      <Routes>
+      <Routes location={background || location}>
         <Route path="/" element={<Login></Login>} />
         <Route path="/home" element={<Home></Home>} />
         <Route path="/registro" element={<Registro></Registro>} />
       </Routes>
+
+      {background && (
+        <Routes>
+          <Route path="/registro" element={<Registro></Registro>} />
+        </Routes>
+      )}
     </>
   );
 }
--- frontend/src/presentation/pages/Login.jsx
+++ frontend/src/presentation/pages/Login.jsx
@@ -1,11 +1,11 @@
 import './login.css';
-import { useNavigate} from "react-router-dom";
 import Button from "../components/Button.jsx";
 import LOGO_X from '../assets/LOGO_X.jpeg';
-
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 export default function Login() {
+    const location = useLocation();
     const navigate = useNavigate();
     return (
         <div className="General">
@@ -13,38 +13,39 @@ export default function Login() {
                 <img src={LOGO_X} alt="Logo X" />
             </div>
             <section className='form-section'>
-            <div className="tittle">
-                <h1>Lo que está pasando ahora</h1>
-            </div>
-            <div className="subtitle">
-                <p>Únete hoy</p>
-            </div>
-            <div className="reg-apps">   
-                <Button label="Registrarse con Google" onClick={()=> {}} variant="primary" type= "button"/>
-            </div>
-            <div className="division">
-                <p>-------------------------O----------------------------</p>
-            </div>
-            <div className="reg-db">
-                <Button label="Crear cuenta" onClick={()=> {navigate("/registro")}} variant="primary" type="button"/>   
-            </div>
-            <div className="privacy">
-                Al registrarte, aceptas nuestras condiciones de uso y política de privacidad, incluida la política de uso de cookies. Es posible que te enviemos correos electrónicos con actualizaciones, ofertas y otra información relacionada con el servicio. Puedes darte de baja de estos correos electrónicos en cualquier momento.
-            </div>
-            <div className="subtitle-2">
-                <p>¿Ya tienes una cuenta? </p>
-            </div>
-            <div className="login">
-                <Button label="Iniciar sesión" onClick={()=> {navigate("/home")}} variant="secondary" type="button" />
-            </div>
+                <div className="tittle">
+                    <h1>Lo que está pasando ahora</h1>
+                </div>
+                <div className="subtitle">
+                    <p>Únete hoy</p>
+                </div>
+                <div className="reg-apps">
+                    <Button label="Registrarse con Google" onClick={() => { }} variant="primary" type="button" />
+                </div>
+                <div className="division">
+                    <p>-------------------------O----------------------------</p>
+                </div>
+                {/* botton crear cuenta */}
+                <div className="reg-db">
+                    <Button label="Crear cuenta" onClick={() => { navigate("/registro", { state: { background: location } }) }} variant="primary" type="button" />
+                </div>
+                <div className="privacy">
+                    Al registrarte, aceptas nuestras condiciones de uso y política de privacidad, incluida la política de uso de cookies. Es posible que te enviemos correos electrónicos con actualizaciones, ofertas y otra información relacionada con el servicio. Puedes darte de baja de estos correos electrónicos en cualquier momento.
+                </div>
+                <div className="subtitle-2">
+                    <p>¿Ya tienes una cuenta? </p>
+                </div>
+                <div className="login">
+                    <Button label="Iniciar sesión" onClick={() => { navigate("/home") }} variant="secondary" type="button" />
+                </div>
             </section>
-            
+
             <div className="footer">
                 Información | Descarga la app de X | Grok | Centro de Ayuda | Condiciones de Servicio | Política de Privacidad | Política de cookies | Accesibilidad | Información de anuncios | Blog
-                 | Empleos | Recursos para marcas | Publicidad | Marketing | X para empresas | Desarrolladores | Noticias | Configuración | © 2026 X Corp.
+                | Empleos | Recursos para marcas | Publicidad | Marketing | X para empresas | Desarrolladores | Noticias | Configuración | © 2026 X Corp.
             </div>
         </div>
-        
+
 
     )
 }
