aa90e431091828b18aaa32407f35d9d16f869440
notardr <ardrnot@gamil.comm>
2026-03-11 16:56:10 -0500

    cambios romero

---
+++ frontend/src/presentation/pages/Inicio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import VistaInicio from '../components/VistaInicio.jsx';
+export default function Inicio() {
+    return (
+        <div>
+           <VistaInicio></VistaInicio>
+        </div>
+    )
+}
