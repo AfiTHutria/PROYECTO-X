db532bbd9dc8720ceeb09aab8bbb23fc182badac
AfiTHutria <afithutria10@gmail.com>
2026-03-11 15:46:03 -0500

    das

--- frontend/src/presentation/pages/Login.jsx
+++ frontend/src/presentation/pages/Login.jsx
@@ -1,19 +1,11 @@
 import './login.css';
-<<<<<<< HEAD
-import { useNavigate, useLocation} from "react-router-dom";
-=======
->>>>>>> 8171530957688570961ea71194f20556f95f8dec
 import Button from "../components/Button.jsx";
 import LOGO_X from '../assets/LOGO_X.jpeg';
 import { useNavigate, useLocation } from "react-router-dom";
 
 
 export default function Login() {
-<<<<<<< HEAD
-    const location= useLocation();
-=======
     const location = useLocation();
->>>>>>> 8171530957688570961ea71194f20556f95f8dec
     const navigate = useNavigate();
     return (
         <div className="General">
@@ -21,32 +13,6 @@ export default function Login() {
                 <img src={LOGO_X} alt="Logo X" />
             </div>
             <section className='form-section'>
-<<<<<<< HEAD
-            <div className="tittle">
-                <h1>Lo que está pasando ahora</h1>
-            </div>
-            <div className="subtitle">
-                <p>Únete hoy</p>
-            </div>
-            <div className="reg-apps">   
-                <Button label="Registrarse con Google" onClick={()=> {}} variant="primary" type= "button"/>
-            </div>
-            <div className="division">
-                <p>-------------------------O----------------------------</p>
-            </div>
-            <div className="reg-db">
-                <Button label="Crear cuenta" onClick={()=> {navigate("/registro",{state: {background: location}})}} variant="primary" type="button"/>   
-            </div>
-            <div className="privacy">
-                Al registrarte, aceptas nuestras condiciones de uso y política de privacidad, incluida la política de uso de cookies. Es posible que te enviemos correos electrónicos con actualizaciones, ofertas y otra información relacionada con el servicio. Puedes darte de baja de estos correos electrónicos en cualquier momento.
-            </div>
-            <div className="subtitle-2">
-                <p>¿Ya tienes una cuenta? </p>
-            </div>
-            <div className="login">
-                <Button label="Iniciar sesión" onClick={()=> {navigate("/home")}} variant="secondary" type="button" />
-            </div>
-=======
                 <div className="tittle">
                     <h1>Lo que está pasando ahora</h1>
                 </div>
@@ -72,7 +38,6 @@ export default function Login() {
                 <div className="login">
                     <Button label="Iniciar sesión" onClick={() => { navigate("/home") }} variant="secondary" type="button" />
                 </div>
->>>>>>> 8171530957688570961ea71194f20556f95f8dec
             </section>
 
             <div className="footer">
